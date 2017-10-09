c3eec6f028f133fc9b8623f3507df4e6fc5a5c9d
Nico Hsieh <zeroevilz@gmail.com>
2017-10-08 18:19:04 -0700

    add physics from oimo

--- src/routes/points/index.js
+++ src/routes/points/index.js
@@ -1,20 +1,21 @@
 import { h, Component } from 'preact'
+import * as OIMO from 'oimo'
 import {
-	Geometry,
-	PointsMaterial,
-	Points,
+	BufferGeometry,
+	BoxBufferGeometry,
+	Mesh,
+	MeshPhongMaterial,
+	MeshBasicMaterial,
 	Vector2,
 	Vector3,
-	Color,
-	AdditiveBlending,
+	PointLight,
+	DirectionalLight,
 	Math as ThreeMath
 } from 'three'
 import { loadTextures } from '../../utils/textureLoader'
 import ThreeContainer from '../../components/three-container'
 
 import style from './style.scss'
-
-const whitePath = '/assets/images/white.png'
 
 
 // use shader material
@@ -27,32 +28,80 @@ export default class Ponts extends Component {
 
 	componentDidMount() {
 		// this.container.scene.background = new Color(0xffffff)
-		loadTextures([whitePath])
-			.then(this.init)
+		this.init()
 	}
 
 	init = (textures) => {
-		this.gemotery = new Geometry()
-		for (let i = 0; i < 1000; i++) {
-			let vertex = new Vector3()
-			vertex.x = ThreeMath.randFloat(-10, 10)
-			vertex.y = ThreeMath.randFloat(-8, 8) + 10
-			vertex.z = ThreeMath.randFloat(-10, 6) - 45
-			this.gemotery.vertices.push(vertex)
-		}
-
-		this.material = new PointsMaterial({
-			map: textures[whitePath],
-			// color: 0xb7fffa,
-			size: 3,
-			alphaTest: 0.1,
-			// transparent: true,
-			blending: AdditiveBlending
+		this.world = new OIMO.World({
+			info: true,
+			worldScale: 100
 		})
 
-		let particles = new Points(this.gemotery, this.material)
-		this.container.scene.add(particles)
+		this.world.gravity = new OIMO.Vec3(0, -3.5, 0)
 
+		const groundSize = [30, 2, 30]
+		const groundPos = [0, -20, 0]
+		this.ground = this.world.add({
+			size: groundSize,
+			pos: groundPos,
+			config: [
+				1, // The density of the shape.
+        0.1, // The coefficient of friction of the shape.
+        0.2, // The coefficient of restitution of the shape.
+        1, // The bits of the collision groups to which the shape belongs.
+        0xffffffff // The bits of the collision groups with which the shape collides.
+			]
+		})
+		
+
+		let groundGeometry = new BoxBufferGeometry(...groundSize)
+		let groundMeterial = new MeshPhongMaterial({
+			color: 0xc6c6c6,
+			emissive: 0x424141
+		})
+		let groundMesh = new Mesh(groundGeometry, groundMeterial)
+		groundMesh.receiveShadow = true
+		groundMesh.castShadow = true
+		groundMesh.position.set(...groundPos)
+
+		this.container.scene.add(groundMesh)
+
+
+		this.meshs = []
+		this.bodys = []
+		const size = 3
+		let geometry = new BoxBufferGeometry(1, 1, 1)
+		let material = new MeshPhongMaterial({
+			color: 0xffffff,
+			emissive: 0xe2004f
+		})
+
+		for(let i = 0; i < 15; i++) {
+			const x = ThreeMath.randFloat(-10, 10)
+			const y = ThreeMath.randFloat(10, 0)
+			const z = ThreeMath.randFloat(-10, 0)
+
+			let body = this.world.add({
+				type:'box', 
+				size: [size, size, size], 
+				pos: [x, y, z], 
+				move: true, 
+				config: [1, 0, 0, 1, 0xffffffff]
+			})
+
+			let mesh = new Mesh(geometry, material)
+			mesh.position.set(x, y, z)
+			mesh.scale.set(size, size, size)
+			mesh.castShadow = true
+			mesh.receiveShadow = true
+			this.container.scene.add(mesh)
+			this.meshs.push(mesh)
+			this.bodys.push(body)
+		}
+   
+  	this.light = new PointLight(0xffffff, 1, 150)
+  	this.light.position.set(0, 5, 5)
+  	this.container.scene.add(this.light)
 		this.inited = true
 	}
 
@@ -67,31 +116,26 @@ export default class Ponts extends Component {
 	}
 
 	animate() {
-		const fc = this.container.frameCount
-		if (!this.inited || fc < 60) {
-			return
-		}
-		const bottom = -25
-		for (let i = 0; i < this.gemotery.vertices.length; i++) {
-			let vertice = this.gemotery.vertices[i]
-			if (vertice.y > bottom) {
-				vertice.y -= ((i % 10) + 1) * 0.01 + 0.17 + fc * 0.008
-			}
-		}
-
-		this.gemotery.verticesNeedUpdate = true
-	}
-
-	switchMode = () => {
 		if (!this.inited) {
 			return
 		}
-		const nextMode = (this.state.mode + 1) % this.totalMode
-		this.setState({
-			mode: nextMode
-		})
-		this.shaderMaterial.uniforms.mode.value = nextMode
+		this.world.step()
+		let body
+		let mesh
+		for(let i = 0; i < this.meshs.length; i++) {
+			body = this.bodys[i]
+			mesh = this.meshs[i]
 
+			if (!body.sleeping) {
+				mesh.position.copy(body.getPosition())
+        mesh.quaternion.copy(body.getQuaternion())
+			}
+
+		}
+	}
+
+	switchMode = () => {
+		
 	}
 
 	render(props, states) {
@@ -102,7 +146,7 @@ export default class Ponts extends Component {
 				</p>
 				<ThreeContainer 
 					ref={el => this.container = el}
-					cameraZPos={1}
+					cameraZPos={50}
 					onMouseMove={this.onMouseMove}
 					onTouchMove={this.onTouchMove}
 					animate={() => {this.animate()}}
